5e1d52c222457f976e71cf45e515c9ec66fefb29
Caleb Depatie <caleb.depatie@outlook.com>
2024-05-27 23:33:06 -0400

    Sqlite query test

--- tests/vendors/sqlite.cpp
+++ tests/vendors/sqlite.cpp
@@ -15,3 +15,38 @@ TEST_CASE("SQLite3 Initialization", "[vendor]") {
 
     sqlite3_close(db);
 }
+
+TEST_CASE("SQLite3 Querying", "[vendor]") {
+    sqlite3 *db;
+    int rc = sqlite3_open(":memory:", &db);
+
+    REQUIRE(rc == SQLITE_OK);
+
+    rc = sqlite3_exec(db, "CREATE TABLE test (id INTEGER PRIMARY KEY, name TEXT);", nullptr, nullptr, nullptr);
+
+    REQUIRE(rc == SQLITE_OK);
+
+    rc = sqlite3_exec(db, "INSERT INTO test (name) VALUES ('Test!');", nullptr, nullptr, nullptr);
+
+    REQUIRE(rc == SQLITE_OK);
+
+    // Prepare the SQL statement
+    sqlite3_stmt *stmt;
+    const char *sql = "SELECT name FROM test;";
+    rc = sqlite3_prepare_v2(db, sql, -1, &stmt, nullptr);
+
+    REQUIRE(rc == SQLITE_OK);
+
+    // Execute the prepared statement
+    while (sqlite3_step(stmt) == SQLITE_ROW) {
+        // Retrieve the value of the "name" column
+        const char *name = reinterpret_cast<const char *>(sqlite3_column_text(stmt, 0));
+        
+        REQUIRE(std::string(name) == "Test!");
+    }
+
+    // Finalize the statement
+    sqlite3_finalize(stmt);
+
+    sqlite3_close(db);
+}
