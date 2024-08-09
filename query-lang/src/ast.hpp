892803db3ee04dfc75d5ec01fe154e2aadfc192a
Conner Tenn <connertenn@gmail.com>
2024-08-08 21:54:15 -0400

    Added in a basic framework for the AST

--- query-lang/src/ast.hpp
+++ query-lang/src/ast.hpp
@@ -2,3 +2,77 @@
 //
 // SPDX-License-Identifier: 0BSD
 
+#include <vector>
+#include <string>
+
+class AstNode
+{
+private:
+    
+
+public:
+    AstNode();
+    virtual ~AstNode();
+};
+
+class BinaryOperator : public AstNode {
+private:
+    AstNode *left_node;
+    AstNode *right_node;
+
+public:
+
+    BinaryOperator();
+    ~BinaryOperator();
+};
+
+
+class UnaryOperator : public AstNode {
+private:
+    AstNode *node;
+
+public:
+
+    UnaryOperator();
+    ~UnaryOperator();
+};
+
+
+class Union : public BinaryOperator {
+
+public:
+    Union();
+    ~Union();
+
+};
+
+
+class Intersection : public BinaryOperator {
+
+public:
+    Intersection();
+    ~Intersection();
+
+};
+
+
+class Negation : public UnaryOperator {
+
+public:
+    Negation();
+    ~Negation();
+
+};
+
+
+class Tag : public AstNode {
+private:
+    std::string name;
+
+public:
+    Tag();
+    ~Tag();
+};
+
+
+
